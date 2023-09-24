6a7e1676023856472173ad5cac050e4ab1ffcfd3
RealKingTino <tmowizzy@gmail.com>
2023-09-24 13:41:39 -0700

    puppet manifest

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Setting up client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  ensure  => 'present',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  ensure  => 'present',
+  replace => true,
+}
