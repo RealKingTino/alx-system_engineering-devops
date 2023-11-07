1ee7eeaa2f242f968666de50b6f027c7ed6bd2f7
RealKingTino <tmowizzy@gmail.com>
2023-11-07 04:32:39 -0800

    Aliyu - Queries_the Reddit_API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Queries the Reddit API and prints the titles
+of the first 10 hot posts listed for
+a given subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    params = {'limit': 10}
+
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
