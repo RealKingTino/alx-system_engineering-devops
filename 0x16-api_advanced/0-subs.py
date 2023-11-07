6fee033379b61dd055f04ec33539290f318992ef
RealKingTino <tmowizzy@gmail.com>
2023-11-07 04:13:31 -0800

    Aliyu - Queries_the Reddit_API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
