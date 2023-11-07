7a3e8a49f7dbd5beb4ae1ef0d97fa7ff3afc6a07
RealKingTino <tmowizzy@gmail.com>
2023-11-07 08:16:20 -0800

    Aliyu - pycodestyle

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
 """
-Queries the Reddit API recursively and returns a list containing the titles of all hot articles for a given subreddit
+Queries the Reddit API recursively and returns a
+list containing the titles of all hot articles
+for a given subreddit
 """
 
 import requests
+
 
 def recurse(subreddit, hot_list=[], after=None):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'MyBot/0.0.1'}
     params = {'limit': 100, 'after': after}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
