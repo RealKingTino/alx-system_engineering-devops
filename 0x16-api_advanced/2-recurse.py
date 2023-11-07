c5c8b78148749fff72809ca09dc6eac833181e23
RealKingTino <tmowizzy@gmail.com>
2023-11-07 08:12:26 -0800

    Aliyu - Queries_the Reddit_API

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Queries the Reddit API recursively and returns a list containing the titles of all hot articles for a given subreddit
+"""
+
+import requests
+
+def recurse(subreddit, hot_list=[], after=None):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    params = {'limit': 100, 'after': after}
+
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        after = data['data']['after']
+
+        for post in posts:
+            hot_list.append(post['data']['title'])
+
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    else:
+        return None
